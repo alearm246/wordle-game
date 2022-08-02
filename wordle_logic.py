b53e8cf6f04107e955449a3b8f50c8f610c65435
alearm246 <alearm246@gmail.com>
2022-08-01 17:26:28 -0700

    added logic for words and added word list

--- wordle_logic.py
+++ wordle_logic.py
@@ -1,3 +1,4 @@
+from words import validWords
 #for storing of functions relating to assigning variables to letters in the boardState
 
 #constants for use when decideing whether to look at letter or legality
@@ -15,6 +16,26 @@ CORRECT_POSITION = (1)
 CORRECT_WRONG_POSITION = (2)
 WRONG = (3)
 
+def isWordValid(guess):
+	lowerCaseGuess = guess.lower()
+	if len(lowerCaseGuess) > 5 or lowerCaseGuess not in validWords:
+		return False
+	return True
 
-def evaluate(board, secretWord):
-	pass
+def evaluate(board, guess, secretWord):
+	board.append([list(guess),[0,0,0,0,0]])
+	boardIndex = board.index([list(guess),[0,0,0,0,0]])
+
+	for i, letter in enumerate(secretWord):
+		if guess[i] in secretWord and letter in guess[i]:
+			board[boardIndex][LEGALITY][i] = CORRECT_POSITION
+		elif guess[i] in secretWord:
+			board[boardIndex][LEGALITY][i] = CORRECT_WRONG_POSITION
+		else:
+			board[boardIndex][LEGALITY][i] = WRONG
+
+
+# #evaluate([], "shoer", "shoer")
+# evaluate([], "shoer", "trues")
+# evaluate([[['s', 'h', 'o', 'e', 'r'], [2, 2, 2, 1, 2]]], "hayer", "trues")
+# evaluate([[['s', 'h', 'o', 'e', 'r'], [2, 2, 2, 1, 2]], [['h', 'a', 'y', 'e', 'r'], [3, 3, 3, 1, 2]]], "trues", "trues")
