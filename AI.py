38b3203eccbf0efc6703b466da1e26a273daa481
phoenyliam <liamswain3@gmail.com>
2022-08-04 03:12:45 -0400

    made cool AI stuff

--- AI.py
+++ AI.py
@@ -31,7 +31,7 @@ def AI_Letter_Count(words):
 	# 			'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, \
 	# 			'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
 	for word in words:
-		print(word)
+		#print(word)
 		myWord = word[:]
 		sorted(myWord)
 		aDict = {}
@@ -48,9 +48,8 @@ def AI_Letter_Count(words):
 					LCDict[coolKey] = 0
 				LCDict[coolKey] += 1
 				aDict[key] -= 1
-	print(LCDict)
+	#print(LCDict)
 def PopularityUpdate(words):
-	AI_Letter_Count(words)
 	for key in PopularityIndex:
 		PopularityIndex[key] = 0
 	for word in words:
@@ -70,15 +69,76 @@ def PopularityUpdate(words):
 					PopularityIndex[word] = 0
 				PopularityIndex[word] += LCDict[coolKey]
 				aDict[key] -= 1
-	print(PopularityIndex)
-# def get_guess(board):
-# 	myDict = {}
-# 	index = 0
-# 	while(board[index][LEGALITY][0] != UNSELECTED):
-# 		for char in board[index][LETTER]:
-# 			if (char not in myDict):
-# 				myDict[char] = 0
-# 			myDict[char] += 1
+	#print(PopularityIndex)
+
 
 def deleteWords(board,words):
-	
+	wordArr = []
+	legal = []
+	for item in board:
+		if(item[LEGALITY][0] == UNSELECTED):
+			break
+		else:
+			print("made")
+			wordArr = item[LETTER][:]
+			legal = item[LEGALITY][:]
+	myDict = {}
+	print(len(wordArr), wordArr)
+	for i in range(len(wordArr)):
+		char = wordArr[i].lower()
+		if char not in myDict:
+			myDict[char] = 0
+		if(legal[i] != WRONG):
+			myDict[char] += 1
+	i = 0
+	result = []
+	while(i < len(words)):
+		dead = False
+		word = words[i]
+		wordDict = {}
+		for char in word:
+			char = char.lower()
+			if char not in wordDict:
+				wordDict[char] = 0
+			wordDict[char] += 1
+		for key in myDict:
+			if key not in wordDict:
+				wordDict[key] = 0
+			if (myDict[key] != wordDict[key]):
+				dead = True
+				break
+
+		print(myDict, wordDict, dead)
+		if dead:
+			print("non-matching: " + word)
+			words.remove(word)
+			continue
+		else:
+			for n in range(len(wordArr)):
+				if(legal[n] == CORRECT_POSITION):
+					if(wordArr[n] == word[n]):
+						dead = True
+						break
+				elif(legal[n] == CORRECT_WRONG_POSITION):
+					if(wordArr[n] == word[n]):
+						dead = True
+						break
+			if(dead == True):
+				print(word)
+				words.remove(i)
+				continue
+			else:
+				i+=1
+				result.append(word)
+	return result
+def get_guess(board,words):
+	words = deleteWords(board,words)
+	AI_Letter_Count(words)
+	PopularityUpdate(words)
+	guess = ""
+	guessNum = 0
+	for key in PopularityIndex:
+		if (PopularityIndex[key] > guessNum):
+			guessNum = PopularityIndex[key]
+			guess = key
+	return words,guess
